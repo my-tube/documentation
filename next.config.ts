d7532ea947f0daca096aec28b4ca74f2c2888c07
Allan Hart <allan.w.hart@gmail.com>
2025-08-05 06:55:31 -0600

    With custom domain, no longer need `assetPrefix` or `basePath`

--- next.config.ts
+++ next.config.ts
@@ -14,8 +14,8 @@ const nextConfig:NextConfig = {
     'docs.mytube.mac',
   ],
 
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/documentation/' : '',
-  basePath: process.env.NODE_ENV === 'production' ? '/documentation' : '',
+  // assetPrefix: process.env.NODE_ENV === 'production' ? '/documentation/' : '',
+  // basePath: process.env.NODE_ENV === 'production' ? '/documentation' : '',
   distDir: 'build',
   images: {
     unoptimized: true, // Recommended for static exports if not using a custom image loader
